5bc77603e343211133444491457d970eafb8a09e
sid <sid@DESKTOP-E47R17I>
2019-08-10 11:48:34 +0530

    testng added project

---
+++ testNG/src/com/java/testng/youtube/UnderstandingTestNG.java
@@ -0,0 +1,13 @@
+package com.java.testng.youtube;
+
+import org.testng.annotations.Test;
+
+public class UnderstandingTestNG
+{
+	@Test
+	public void testng()
+	{
+			System.out.println("Understanding TestNG.xml file");
+	}
+
+}
---
+++ testNG/src/com/java/testng/youtube/UnderstandingTestNG2.java
@@ -0,0 +1,13 @@
+package com.java.testng.youtube;
+
+import org.testng.annotations.Test;
+
+public class UnderstandingTestNG2
+{
+	@Test
+	public void testng1()
+	{
+			System.out.println("Understanding TestNG.xml file2");
+	}
+
+}
